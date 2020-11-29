9bf210f4a4202d219d0f04208b5136c6197c8679
habib <habib.khademhosseini@gmail.com>
2020-11-29 15:43:22 +0330

    add new files

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.scss';
+const Header = (props) => {
+  return (
+    <header>
+      <div className='wrap'>
+        <div className='logo'>LOGO</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
